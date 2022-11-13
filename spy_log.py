7b75683058f8840c2f0deeb8c22d0856447e28b0
[иiiiivanchm <[ iiiivanchm@yandex.ru>
2022-11-13 07:00:11 +0300

    reform db.csv

--- spy_log.py
+++ spy_log.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 
 
 def log(update: Update, context: CallbackContext):
-    file = open('db.csv', 'a')
-    file.write(
-        f'{update.effective_user.first_name}, {update.effective_user.id}, {update.message.text}\n')
-    file.close()
+    with open('db.csv', 'a') as f:
+        f.write(
+            f'{update.effective_user.first_name}, {update.effective_user.id}, {update.message.text}\n')
+
